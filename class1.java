660051aa2009de2fcbd112cd6e086398d6064c90
a.gerganov <gerganovalexander@gmail.com>
2023-06-27 11:55:53 +0300

    changes 1

--- class1.java
+++ class1.java
@@ -1,2 +1,33 @@
 public class class1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Smile usmivka");
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Second for loop");
+        }
+        for (int i = 0; i < 7; i++) {
+            System.out.println("");
+        }
+
+
+        addNumbers(5,8);
+
+
+    }
+    public static int addNumbers(int a, int c) {
+        int sum = a + c;
+        // return valueok
+        return sum;
+    }
+
+    public static int abs(int a){
+        System.out.println(a);
+        return Math.abs(a);
+    }
+
+    public static double negative(int a){
+        return -a - 1;
+    }
+
 }

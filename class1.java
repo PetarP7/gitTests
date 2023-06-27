64d983c0c50197c60ac977671b5e337f1ed7033c
y.bozhilov <yordanbbozhilov@gmail.com>
2023-06-27 11:04:53 +0300

    First commit

--- class1.java
+++ class1.java
@@ -1,2 +1,24 @@
 public class class1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Smile usmivka");
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Second for loop");
+        }
+        for (int i = 0; i < 7; i++) {
+            System.out.println("");
+        }
+
+
+        addNumbers(5,8);
+
+
+    }
+    public static int addNumbers(int a, int b) {
+        int sum = a + b;
+        // return valueok
+        return sum;
+    }
+
 }
